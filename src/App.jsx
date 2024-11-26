7fa953a60c262f15cef22844bd384fa739bcc39e
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-11-26 22:30:40 +0600

    remove function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import Header from './components/Header'
 
+function WayToTeach() {
+	return (
+		<li>
+			<p>
+				<strong>Индивидуализация процесса</strong>Каждый ученик
+				<br />
+				уникален, поэтому мы разрабатываем <br />
+				персонализированные
+				<br />
+				программы, учитывая уровень подготовки, цели и <br />
+				предпочтения
+				<br />
+			</p>
+		</li>
+	)
+}
+
 export default function App() {
 	return (
 		<div>
 			<Header />
 			<main>
-				<h1>Hello React!</h1>
+				<section>
+					<h3>Наш подход к обучению</h3>
+					<ul>
+						<WayToTeach />
+						<WayToTeach />
+						<WayToTeach />
+					</ul>
+				</section>
 			</main>
 		</div>
 	)
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
+import logo from '/vite.svg'
+
 export default function Header() {
+	const now = new Date()
 	return (
 		<header>
-			<h3>Чукур</h3>
-			<span>Здесь будет время</span>
+			{/* <h3>Чукур</h3> */}
+			<img src={logo} alt={'Logo'} />
+			<span>Время сейчас :{now.toLocaleTimeString()} </span>
 		</header>
 	)
 }
